40cd2aa678bf1e8f0757f7652b0c9f0fd392bbf2
Alexander Novikov <novikov@bayesgroup.ru>
2017-01-19 20:50:56 +0300

    convert_to_tt_array -> to_tt_array (shorter namem)

--- t3f_test.py
+++ t3f_test.py
@@ -9,7 +9,7 @@ class TTArrayTest(tf.test.TestCase):
         vec_shape = (2, 3, 4, 3)
         np.random.seed(1)
         vec = np.random.rand(*vec_shape).astype(np.float32)
-        tt_vec = t3f.convert_to_tt_array(vec)
+        tt_vec = t3f.to_tt_array(vec)
         self.assertAllClose(t3f.full(tt_vec), vec)
 
 
