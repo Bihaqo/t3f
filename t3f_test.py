f91ae10e7ea2d62366ff55c15044f17c3ac7305b
Alexander Novikov <novikov@bayesgroup.ru>
2017-01-19 22:35:55 +0300

    Change tt_array -> tt_matrix

--- t3f_test.py
+++ t3f_test.py
@@ -3,32 +3,32 @@ import tensorflow as tf
 
 import t3f
 
-class TTArrayTest(tf.test.TestCase):
+class TTMatrixTest(tf.test.TestCase):
 
     def testTTVector(self):
         vec_shape = (2, 3, 4, 3)
         np.random.seed(1)
         vec = np.random.rand(*vec_shape).astype(np.float32)
-        tt_vec = t3f.to_tt_array(vec, vec_shape)
+        tt_vec = t3f.to_tt_matrix(vec, vec_shape)
         # TODO: test full and to_tt_array separately?
-        self.assertAllClose(t3f.full(tt_vec), vec)
+        self.assertAllClose(t3f.full_matrix(tt_vec), vec)
 
         vec = np.random.rand(np.prod(vec_shape)).astype(np.float32)
-        tt_vec = t3f.to_tt_array(vec, vec_shape)
+        tt_vec = t3f.to_tt_matrix(vec, vec_shape)
         # TODO: test full and to_tt_array separately?
-        self.assertAllClose(t3f.full(tt_vec), vec)
+        self.assertAllClose(t3f.full_matrix(tt_vec), vec)
 
 
-    def testTTVector(self):
+    def testTTMatrix(self):
         # Convert a np.prod(out_shape) x np.prod(in_shape) matrix into TT-matrix
         # and back.
         inp_shape = (2, 3, 4, 3)
         out_shape = (3, 3, 3, 3)
         np.random.seed(1)
         mat = np.random.rand(np.prod(out_shape), np.prod(inp_shape)).astype(np.float32)
-        tt_mat = t3f.to_tt_array(mat, ((out_shape, inp_shape)))
+        tt_mat = t3f.to_tt_matrix(mat, ((out_shape, inp_shape)))
         # TODO: test full and to_tt_array separately?
-        self.assertAllClose(t3f.full(tt_mat), mat)
+        self.assertAllClose(t3f.full_matrix(tt_mat), mat)
 
 
 if __name__ == "__main__":
