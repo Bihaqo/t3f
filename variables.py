70ac0545a24376c21b21f641747949f681cf762e
Alexander Novikov <novikov@bayesgroup.ru>
2017-01-24 16:56:41 +0300

    Add variable initializer api

---
+++ variables.py
@@ -0,0 +1,13 @@
+import tensorflow as tf
+
+def get_tt_variable(name,
+                 shape=None,
+                 dtype=None,
+                 initializer=None,
+                 regularizer=None,
+                 trainable=True,
+                 collections=None,
+                 caching_device=None,
+                 validate_shape=True):
+  # Returns TensorTrain object with tf.Variables as the TT-cores.
+  raise NotImplementedError
