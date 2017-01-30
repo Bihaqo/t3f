6acbf6566e8a10ceda8f3336cb9952b154e1e5ff
Alexander Novikov <novikov@bayesgroup.ru>
2017-01-30 12:37:36 +0300

    Support regularization

--- variables.py
+++ variables.py
@@ -20,7 +20,9 @@ def get_tt_variable(name,
     dtype: Type of the new or existing TensorTrain variable TT-cores (defaults
       to DT_FLOAT).
     initializer: Initializer for the variable if one is created.
-    regularizer: Not supported yet.
+    regularizer: A (TensorTrain -> Tensor or None) function; the result of
+      applying it on a newly created variable will be added to the collection
+      GraphKeys.REGULARIZATION_LOSSES and can be used for regularization.
     trainable: If True also add the variable to the graph collection
       GraphKeys.TRAINABLE_VARIABLES (see tf.Variable).
     collections:  List of graph collections keys to add the Variables
@@ -43,22 +45,11 @@ def get_tt_variable(name,
       violating reuse during variable creation, or when initializer dtype and
       dtype don't match. Reuse is set inside variable_scope.
   """
-  # TODO: support regularizer (a TensorTrain -> Tensor function).
   # TODO: Provide basic regularizers (like apply_to_cores(func)).
 
   variable_cores = []
-  if initializer is not None:
-    with tf.variable_scope(name):
-      num_dims = initializer.ndims()
-      for i in range(num_dims):
-        curr_core_var = tf.get_variable('core_%d' % i,
-                                        initializer=initializer.tt_cores[i],
-                                        dtype=dtype, trainable=trainable,
-                                        collections=collections,
-                                        caching_device=caching_device,
-                                        validate_shape=validate_shape)
-        variable_cores.append(curr_core_var)
-  else:
+  if initializer is None:
+    # Find an existing variable.
     with tf.variable_scope(name):
       i = 0
       while True:
@@ -77,7 +68,31 @@ def get_tt_variable(name,
           else:
             # We found all the cores, the i-th core doesn't exist.
             break
-  return tensor_train.TensorTrain(variable_cores, convert_to_tensors=False)
+    v = tensor_train.TensorTrain(variable_cores, convert_to_tensors=False)
+  else:
+    # Create new variable.
+    with tf.variable_scope(name):
+      num_dims = initializer.ndims()
+      for i in range(num_dims):
+        curr_core_var = tf.get_variable('core_%d' % i,
+                                        initializer=initializer.tt_cores[i],
+                                        dtype=dtype, trainable=trainable,
+                                        collections=collections,
+                                        caching_device=caching_device,
+                                        validate_shape=validate_shape)
+        variable_cores.append(curr_core_var)
+    v = tensor_train.TensorTrain(variable_cores, convert_to_tensors=False)
+
+    # Run the regularizer if requested and save the resulting loss.
+    if regularizer:
+      with tf.name_scope(name + "/Regularizer/"):
+        loss = regularizer(v)
+      if loss is not None:
+        tf.logging.vlog(1, "Applied regularizer to %s and added the result %s "
+                        "to REGULARIZATION_LOSSES.", v.name, loss.name)
+        tf.add_to_collection(tf.GraphKeys.REGULARIZATION_LOSSES, loss)
+
+  return v
 
 
 def assign(ref, value, validate_shape=None, use_locking=None, name=None):
