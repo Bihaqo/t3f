4ba3dfc2f61f0881797c20baa7f6bb72c3566930
Alexander Novikov <novikov@bayesgroup.ru>
2017-02-03 14:53:37 +0300

    s/rank/tt_rank/ in the tests + a comment

--- variables_test.py
+++ variables_test.py
@@ -8,7 +8,7 @@ import initializers
 class VariablesTest(tf.test.TestCase):
 
   def testGetExistingVariable(self):
-    init = initializers.tt_rand_tensor([2, 3, 2], rank=2)
+    init = initializers.tt_rand_tensor([2, 3, 2], tt_rank=2)
     tt_1 = variables.get_tt_variable('tt_1', initializer=init)
     # Check that we can create another variable without name
     # conflict (ValueError).
@@ -23,15 +23,18 @@ class VariablesTest(tf.test.TestCase):
         self.assertAllClose(ops.full(tt_1).eval(), ops.full(tt_1_copy).eval())
 
   def testAssign(self):
-    old_init = initializers.tt_rand_tensor([2, 3, 2], rank=2)
+    old_init = initializers.tt_rand_tensor([2, 3, 2], tt_rank=2)
     tt = variables.get_tt_variable('tt', initializer=old_init)
-    new_init = initializers.tt_rand_tensor([2, 3, 2], rank=4)
+    # The ranks of old_init and new_init should be different to check that
+    # assign correctly changes the shape of the TT-cores.
+    new_init = initializers.tt_rand_tensor([2, 3, 2], tt_rank=4)
     assigner = variables.assign(tt, new_init)
     with self.test_session():
       tf.global_variables_initializer().run()
       init_value = ops.full(tt).eval()
       assigner_value = ops.full(assigner).eval()
-      after_value = ops.full(tt).eval()
+      after_value = ops.full(tt)
+      after_value = after_value.eval()
       self.assertAllClose(assigner_value, after_value)
       # Assert that the value actually changed:
       abs_diff = np.linalg.norm((init_value - after_value).flatten())
