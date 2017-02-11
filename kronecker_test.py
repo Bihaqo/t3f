6e252b6d73762df366d03c76ed0f387b8934b271
Pavel Izmailov <izmailovpavel@gmail.com>
2017-02-11 19:16:38 +0300

    added test for determinant

--- kronecker_test.py
+++ kronecker_test.py
@@ -22,5 +22,18 @@ class KroneckerTest(tf.test.TestCase):
     kron_mat = t3f.get_variable('kron_mat', initializer=t3f.random_matrix(((2, 3), (3, 2)), tt_rank=1))
     self.assertTrue(kr._is_kron(kron_mat))
 
+  def testDet(self):
+    """
+    Tests the determinant function
+    """
+    tf.set_random_seed(5)
+    kron_mat = t3f.get_variable('kron_mat', initializer=t3f.random_matrix(((2, 3, 2), (2, 3, 2)), tt_rank=1))
+    init_op = tf.global_variables_initializer()
+    with self.test_session() as sess:
+      sess.run(init_op)
+      desired = np.linalg.det(ops.full(kron_mat).eval())
+      actual = kr.determinant(kron_mat)
+      self.assertAllClose(desired, actual.eval())
+
 if __name__ == "__main__":
   tf.test.main()
