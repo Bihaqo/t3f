924f287e13661c72d3daa976b74ab20673e2f0c4
Alexander Novikov <novikov@bayesgroup.ru>
2017-03-12 09:57:30 +0300

    Init setup script

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='t3f',
+      version='0.1',
+      description='Tensor Train decomposition on TensorFlow',
+      url='https://github.com/Bihaqo/t3f',
+      author='Alexander Novikov',
+      author_email='sasha.v.novikov@gmail.com',
+      license='MIT',
+      packages=['t3f'],
+      zip_safe=False)
