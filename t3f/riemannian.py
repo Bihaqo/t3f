630d559ce90c6975f55e4c3c9c8e3f347f1b17b8
Alexander Novikov <novikov@bayesgroup.ru>
2017-03-21 16:59:39 +0300

    Finish sketch of projection implementation

--- t3f/riemannian.py
+++ t3f/riemannian.py
@@ -15,6 +15,7 @@ def project(tangent_space_tens, tensor, coef=None):
   This function implements an algorithm from the paper [1], theorem 3.1.
 
   [1] C. Lubich, I. Oseledets and B. Vandereycken, Time integration of
+    Tensor Trains
 
   Args:
     tangent_space_tens: TensorTrain.
@@ -22,7 +23,7 @@ def project(tangent_space_tens, tensor, coef=None):
     projection of the sum of elements in the batch.
 
   Returns:
-     a TensorTraain with the TT-ranks equal 2 * rank(tangent_space_tens).
+     a TensorTrain with the TT-ranks equal 2 * rank(tangent_space_tens).
   """
   # Always work with batch of TT for simplicity.
   tensor = shapes.expand_batch_dim(tensor)
@@ -32,36 +33,24 @@ def project(tangent_space_tens, tensor, coef=None):
     raise ValueError('The first argument should be a TT-tensor (not matrix), '
                      'got "%s".' % tangent_space_tens)
 
-
   if tangent_space_tens.get_raw_shape() != tensor.get_raw_shape():
     raise ValueError('The shapes of the tensor we want to project and of the '
                      'tensor on which tangent space we want to project should '
-                     'match, got %d %d.' % (tangent_space_tens.get_raw_shape(),
-                                            tensor.get_raw_shape()))
+                     'match, got %s and %s.' %
+                     (tangent_space_tens.get_raw_shape(),
+                      tensor.get_raw_shape()))
 
-  # Right to left orthogonalization of tangent_space_tens.
-  tangent_space_tens = decompositions.orthogonalize_tt_cores(tangent_space_tens,
-                                                             left_to_right=False)
+  left_tangent_space_tens = decompositions.orthogonalize_tt_cores(
+    tangent_space_tens)
+  right_tangent_space_tens = decompositions.orthogonalize_tt_cores(
+    left_tangent_space_tens, left_to_right=False)
+
 
   ndims = tangent_space_tens.ndims()
   shape = shapes.lazy_raw_shape(tangent_space_tens)
   batch_size = shapes.lazy_batch_size(tensor)
-  tangent_tt_ranks = shapes.lazy_tt_ranks(tangent_space_tens)
-  tensor_tt_ranks = shapes.lazy_tt_ranks(tensor)
-
-  # Initialize the cores of the projection_X(sum z[i]).
-  res_ranks = []
-  for core_idx in range(ndims + 1):
-    if core_idx == 0 or core_idx == ndims - 1:
-      res_ranks.append(1)
-    else:
-      res_ranks.append(2 * tangent_tt_ranks[core_idx])
-  res_cores = []
-  for core_idx in range(ndims):
-    res_cores.append(tf.zeros((res_ranks[core_idx], shape[core_idx],
-                               res_ranks[core_idx + 1])))
-    for value in range(modeSize[dim]):
-      res_cores[core_idx, 0:r1, value, 0:r2] = tangent_space_tens.tt_cores[core_idx, :, value, :]
+  right_tangent_tt_ranks = shapes.lazy_tt_ranks(right_tangent_space_tens)
+  left_tangent_tt_ranks = shapes.lazy_tt_ranks(left_tangent_space_tens)
 
   # Prepare rhs vectors.
   # rhs[core_idx] is of size
@@ -70,47 +59,44 @@ def project(tangent_space_tens, tensor, coef=None):
   rhs[ndims] = tf.ones((batch_size, 1, 1))
   for core_idx in range(ndims - 1, 0, -1):
     tens_core = tensor.tt_cores[core_idx]
-    tang_core = tangent_space_tens.tt_cores[core_idx]
-    rhs[core_idx] = tf.einsum('oaib,cid,obd->oac', tens_core, tang_core,
+    right_tang_core = right_tangent_space_tens.tt_cores[core_idx]
+    rhs[core_idx] = tf.einsum('oaib,cid,obd->oac', tens_core, right_tang_core,
                               rhs[core_idx + 1])
 
   # lhs[core_idx] is of size
   #   batch_size x tangent_tt_ranks[core_idx] x tensor_tt_ranks[core_idx]
   lhs = tf.ones((batch_size, 1, 1))
   # Left to right sweep.
+  res_cores_list = []
   for core_idx in range(ndims):
-    tang_core = tangent_space_tens.tt_cores[core_idx]
-    # r1, n, r2 = cc.shape
+    tens_core = tensor.tt_cores[core_idx]
+    left_tang_core = left_tangent_space_tens.tt_cores[core_idx]
+    right_tang_core = right_tangent_space_tens.tt_cores[core_idx]
+
     if core_idx < ndims - 1:
-      # Left to right orthogonalization.
-      cc = tf.reshape(cc, (-1, r2))
-      cc, rr = np.linalg.qr(cc)
-      r2 = cc.shape[1]
-      # Warning: since ranks can change here, do not use X.r!
-      # Use coresX[dim].shape instead.
-      coresX[dim] = reshape(cc, (r1, n, r2)).copy()
-      coresX[dim + 1] = np.tensordot(rr, coresX[dim + 1], 1)
+      new_lhs = tf.einsum('oab,aic,obid->ocd', lhs, left_tang_core, tens_core)
 
-      new_lhs = tf.einsum('oaib,cid,oac->obd', tens_core, tang_core, lhs)
-
-      currPCore = tf.einsum('oab,obic->oaic', lhs, tens_core)
-      # currPCore = tf.einsum('ijk,iklm->ijlm', lhs, zCoresDim[dim])
-      currPCore = tf.reshape(currPCore, (len(zArr), r1 * n, -1))
-      currPCore -= tf.einsum('ij,kjl->kil', cc, new_lhs)
-      currPCore = tf.einsum('ijk,ikl', currPCore, rhs[dim + 1])
-      currPCore = tf.reshape(currPCore, (r1, modeSize[dim], r2))
-      if core_idx == 0:
-        res_cores[core_idx][0:r1, :, 0:r2] += currPCore
-      else:
-        res_cores[core_idx][r1:, :, 0:r2] += currPCore
+      proj_core = tf.einsum('oab,obic->oaic', lhs, tens_core)
+      proj_core -= tf.einsum('aib,obc->oaic', right_tang_core, new_lhs)
+      # TODO: add coef here.
+      proj_core = tf.einsum('oaib,obc->aic', proj_core, rhs[core_idx + 1])
       lhs = new_lhs
 
-      if core_idx == 0:
-        res_cores[core_idx][0:r1, :, r2:] = tang_core
-      else:
-        res_cores[core_idx][r1:, :, r2:] = tang_core
-
     if core_idx == ndims - 1:
-      res_cores[core_idx][r1:, :, 0:r2] += tf.einsum('oab,obic->oaic', lhs, tens_core)
+      proj_core = tf.einsum('oab,obic->aic', lhs, tens_core)
 
-  return TensorTrain(res_cores)
+    if core_idx == 0:
+      res_core = tf.concat((proj_core, left_tang_core), axis=2)
+    elif core_idx == ndims - 1:
+      res_core = tf.concat((right_tang_core, proj_core), axis=0)
+    else:
+      mode_size = shape[core_idx]
+      rank_1 = right_tangent_tt_ranks[core_idx]
+      rank_2 = left_tangent_tt_ranks[core_idx + 1]
+      zeros = tf.zeros(rank_1, mode_size, rank_2)
+      upper = tf.concat((right_tang_core, zeros), axis=2)
+      lower = tf.concat((proj_core, left_tang_core), axis=2)
+      res_core = tf.concat((upper, lower), axis=0)
+    res_cores_list.append(res_core)
+  # TODO: TT-ranks.
+  return TensorTrain(res_cores_list, tangent_space_tens.get_raw_shape())
