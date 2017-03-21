f75c39b0c1d6583a40029b22cd2634941a91867a
Alexander Novikov <novikov@bayesgroup.ru>
2017-03-21 17:19:15 +0300

    Typos fixes

--- t3f/riemannian_test.py
+++ t3f/riemannian_test.py
@@ -11,8 +11,9 @@ class RiemannianTest(tf.test.TestCase):
     tens = initializers.random_tensor((2, 3, 4))
     # Projection of X into the tangent space of itself is X: P_x(x) = x.
     proj = riemannian.project(tens, tens)
-    with self.test_session():
-      self.assertAllClose(ops.full(proj), ops.full(tens))
+    with self.test_session() as sess:
+      actual_val, desired_val = sess.run((ops.full(proj), ops.full(tens)))
+      self.assertAllClose(desired_val, actual_val)
 
 if __name__ == "__main__":
   tf.test.main()
