b50f3898ab7adaa708ab8cdde1c2881993c8568a
Alexander Novikov <novikov@bayesgroup.ru>
2017-03-22 12:13:01 +0300

    Matrix generation typo

--- t3f/riemannian_test.py
+++ t3f/riemannian_test.py
@@ -83,7 +83,7 @@ class RiemannianTest(tf.test.TestCase):
   def testProjectMatrixOnItself(self):
     # Project a TT-matrix on itself.
     # Projection of X into the tangent space of itself is X: P_x(x) = x.
-    tt_mat = initializers.random_matrix((2, 3, 4))
+    tt_mat = initializers.random_matrix(((2, 3, 4), (2, 3, 4)))
     proj = riemannian.project(tt_mat, tt_mat)
     with self.test_session() as sess:
       actual_val, desired_val = sess.run((ops.full(proj), ops.full(tt_mat)))
