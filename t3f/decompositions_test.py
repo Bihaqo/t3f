43b1bfa3e44adba0003c25b7a0de120bb1adcb6f
Alexander Novikov <novikov@bayesgroup.ru>
2017-03-22 16:23:23 +0300

    Test batch orthogonalization

--- t3f/decompositions_test.py
+++ t3f/decompositions_test.py
@@ -133,5 +133,31 @@ class DecompositionsTest(tf.test.TestCase):
                             should_be_eye_val)
 
 
+class DecompositionsBatchTest(tf.test.TestCase):
+
+  def testOrthogonalizeLeftToRight(self):
+    shape = (2, 4, 3, 3)
+    tt_ranks = (1, 5, 2, 17, 1)
+    updated_tt_ranks = (1, 2, 2, 6, 1)
+    tens = initializers.random_tensor_batch(shape, tt_rank=tt_ranks,
+                                            batch_size=2)
+    orthogonal = decompositions.orthogonalize_tt_cores(tens)
+    with self.test_session() as sess:
+      tens_val, orthogonal_val = sess.run([ops.full(tens), ops.full(orthogonal)])
+      self.assertAllClose(tens_val, orthogonal_val, atol=1e-5, rtol=1e-5)
+      dynamic_tt_ranks = shapes.tt_ranks(orthogonal).eval()
+      self.assertAllEqual(updated_tt_ranks, dynamic_tt_ranks)
+      # Check that the TT-cores are orthogonal.
+      for core_idx in range(4 - 1):
+        core_shape = (updated_tt_ranks[core_idx] * shape[core_idx],
+                      updated_tt_ranks[core_idx + 1])
+        for i in range(2):
+          core = tf.reshape(orthogonal.tt_cores[core_idx][i], core_shape)
+          should_be_eye = tf.matmul(tf.transpose(core), core)
+          should_be_eye_val = sess.run(should_be_eye)
+          self.assertAllClose(np.eye(updated_tt_ranks[core_idx + 1]),
+                              should_be_eye_val)
+
+
 if __name__ == "__main__":
   tf.test.main()
