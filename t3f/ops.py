20c7ed691f2615b1bfbbf30e1cba6530bbf59775
Alexander Novikov <novikov@bayesgroup.ru>
2017-05-22 17:20:53 +0300

    Implement add_n

--- t3f/ops.py
+++ t3f/ops.py
@@ -940,3 +940,27 @@ def cast(tt_a, dtype):
   else:
     raise ValueError('Unsupported type of input "%s", should be TensorTrain or '
                      'TensorTrainBatch.' % tt_a)
+
+
+def add_n(tt_objects, max_r):
+  """Adds a bunch of TT-object and round after each summation.
+  
+  This version implements a slow-to-compile but fast-to-execute (at least on
+  a GPU) version: summing in a binary tree order.
+
+  Args:
+    tt_objects: a list of `TensorTrainBase` objects.
+  
+  Returns:
+    Object of the same type as each input.
+  """
+  prev_level = tt_objects
+  while len(prev_level) > 1:
+    next_level = []
+    for i in range(0, len(prev_level), 2):
+      curr = prev_level[i]
+      if i + 1 < len(prev_level):
+        curr = decompositions.round(curr + prev_level[i + 1], max_r)
+      next_level.append(curr)
+    prev_level = next_level
+  return prev_level[0]
