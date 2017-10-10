1b04c2e5c15d9c903243ae601d9ece3aa02d1a28
Alexander Novikov <novikov@bayesgroup.ru>
2017-10-10 12:18:18 +0300

    Manually set all functions that should be imported

--- t3f/__init__.py
+++ t3f/__init__.py
@@ -1,11 +1,58 @@
 from t3f.tensor_train_base import TensorTrainBase
 from t3f.tensor_train import TensorTrain
 from t3f.tensor_train_batch import TensorTrainBatch
-from t3f.variables import *
-from t3f.ops import *
-from t3f.batch_ops import *
-from t3f.initializers import *
-from t3f.regularizers import *
-from t3f.riemannian import *
-from t3f.shapes import *
-from t3f.decompositions import *
+
+from t3f.variables import assign
+from t3f.variables import get_variable
+
+from t3f.ops import add
+from t3f.ops import cast
+from t3f.ops import flat_inner
+from t3f.ops import frobenius_norm
+from t3f.ops import frobenius_norm_squared
+from t3f.ops import full
+from t3f.ops import matmul
+from t3f.ops import multiply
+from t3f.ops import quadratic_form
+from t3f.ops import sparse_tt_flat_inner
+from t3f.ops import sparse_tt_matmul
+from t3f.ops import transpose
+
+from t3f.batch_ops import concat_along_batch_dim
+from t3f.batch_ops import gram_matrix
+from t3f.batch_ops import multiply_along_batch_dim
+from t3f.batch_ops import pairwise_flat_inner
+
+from t3f.initializers import random_matrix
+from t3f.initializers import random_matrix_batch
+from t3f.initializers import random_tensor
+from t3f.initializers import random_tensor_batch
+
+from t3f.regularizers import cores_regularizer
+from t3f.regularizers import l2_regularizer
+
+from t3f.riemannian import add_n_projected
+from t3f.riemannian import pairwise_flat_inner_projected
+from t3f.riemannian import project
+from t3f.riemannian import project_matmul
+from t3f.riemannian import project_sum
+
+from t3f.shapes import batch_size
+from t3f.shapes import clean_raw_shape
+from t3f.shapes import expand_batch_dim
+from t3f.shapes import is_batch_broadcasting_possible
+from t3f.shapes import lazy_batch_size
+from t3f.shapes import lazy_raw_shape
+from t3f.shapes import lazy_shape
+from t3f.shapes import lazy_tt_ranks
+from t3f.shapes import raw_shape
+from t3f.shapes import shape
+from t3f.shapes import squeeze_batch_dim
+from t3f.shapes import tt_ranks
+
+from t3f.decompositions import orthogonalize_tt_cores
+from t3f.decompositions import round
+from t3f.decompositions import to_tt_matrix
+from t3f.decompositions import to_tt_tensor
+
+import kronecker
