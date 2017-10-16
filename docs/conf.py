b2a873d09be395557d2e91786bf4bfdaaf37f1ce
Alexander Novikov <novikov@bayesgroup.ru>
2017-10-16 11:27:46 +0300

    Add Valentin Khrulkov to authors

--- docs/conf.py
+++ docs/conf.py
@@ -50,8 +50,8 @@ master_doc = 'index'
 
 # General information about the project.
 project = u't3f'
-copyright = u'2017, Alexander Novikov, Pavel Izmailov, Ivan Oseledets, Mikhail Trofimov'
-author = u'Alexander Novikov, Pavel Izmailov, Ivan Oseledets, Mikhail Trofimov'
+copyright = u'2017, Alexander Novikov, Pavel Izmailov, Ivan Oseledets, Mikhail Trofimov, Valentin Khrulkov'
+author = u'Alexander Novikov, Pavel Izmailov, Ivan Oseledets, Mikhail Trofimov, Valentin Khrulkov'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
