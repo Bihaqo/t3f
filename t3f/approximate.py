e73ee4a029ae4856c9f2060f5852e2be36986c8a
Alexander Novikov <novikov@bayesgroup.ru>
2017-12-05 23:25:39 +0300

    Init batch version of sum_round

--- t3f/approximate.py
+++ t3f/approximate.py
@@ -1,3 +1,5 @@
+import tensorflow as tf
+from t3f.tensor_train_batch import TensorTrainBatch
 from t3f import decompositions
 
 
@@ -23,4 +25,59 @@ def add_n(tt_objects, max_tt_rank):
         curr = decompositions.round(curr + prev_level[i + 1], max_tt_rank)
       next_level.append(curr)
     prev_level = next_level
+  return prev_level[0]
+
+
+def reduce_sum_batch(tt_batch, max_tt_rank):
+  """Sum of all TT-objects in the batch with rounding after each summation.
+  
+  
+  This version implements a slow-to-compile but fast-to-execute (at least on
+  a GPU) version: summing in a binary tree order.
+
+  Args:
+    tt_batch: `TensorTrainBatch` object.
+    max_tt_rank: a number, TT-rank for each individual rounding.
+
+  Returns:
+    A `TensorTrain` object representing element-wise sum of all the objects in
+    the batch.
+  """
+  ndims = tt_batch.ndims()
+  left_tt_rank_dim = tt_batch.left_tt_rank_dim
+  right_tt_rank_dim = tt_batch.right_tt_rank_dim
+  tt_ranks = tt_batch.get_tt_ranks()
+  shape = tt_batch.get_shape().as_list()
+  dtype = tt_batch.dtype
+
+  prev_level = tt_batch
+  while prev_level.batch_size > 1:
+    current_level_cores = []
+    for core_idx in range(ndims):
+      curr_orig_core = prev_level.tt_cores[core_idx]
+      a_core = curr_orig_core[::2]
+      b_core = curr_orig_core[1::2]
+      if a_core.get_shape()[0] < b_core.get_shape()[0]:
+        # Not even number of elements in the batch, will have to add dummy
+        # TT-object with the tt-cores filled with zeros.
+        zeros = tf.zeros((1, b_core.get_shape().as_list()[1:]))
+        b_core = tf.concat((b_core, zeros), axis=0)
+
+      if core_idx == 0:
+        curr_sum_core = tf.concat((a_core, b_core), axis=right_tt_rank_dim)
+      elif core_idx == ndims - 1:
+        curr_sum_core = tf.concat((a_core, b_core), axis=left_tt_rank_dim)
+      else:
+        upper_zeros = tf.zeros((tt_ranks[core_idx], shape[0][core_idx],
+                                shape[1][core_idx], tt_ranks[core_idx + 1]),
+                               dtype)
+        lower_zeros = tf.zeros((tt_ranks[core_idx], shape[0][core_idx],
+                                shape[1][core_idx], tt_ranks[core_idx + 1]),
+                               dtype)
+        upper = tf.concat((a_core, upper_zeros), axis=right_tt_rank_dim)
+        lower = tf.concat((lower_zeros, b_core), axis=right_tt_rank_dim)
+        curr_sum_core = tf.concat((upper, lower), axis=left_tt_rank_dim)
+      current_level_cores.append(curr_sum_core)
+    current_level = TensorTrainBatch(current_level, shape, tt_ranks)
+    prev_level = decompositions.round(current_level, max_tt_rank)
   return prev_level[0]
--- t3f/approximate_test.py
+++ t3f/approximate_test.py
@@ -35,6 +35,24 @@ class ApproximateTest(tf.test.TestCase):
       res_desired_val, res_actual_val = sess.run([res_desired, res_actual])
       self.assertAllClose(res_desired_val, res_actual_val, atol=1e-5, rtol=1e-5)
 
+  def testReduceSumBatch(self):
+    # Sum a batch of TT-tensors.
+
+    def desired(tt_batch):
+      res = tt_batch[0]
+      for i in range(1, tt_batch.batch_size):
+        res += tt_batch[i]
+      return res
+    for batch_size in [2, 3, 4, 5]:
+      with self.test_session() as sess:
+        tt_batch = initializers.random_tensor_batch((4, 6, 8, 2),
+                                                    tt_rank=2,
+                                                    batch_size=batch_size)
+        res_actual = ops.full(approximate.reduce_sum_batch(tt_batch, 6))
+        res_desired = ops.full(desired(tt_batch))
+        res_desired_val, res_actual_val = sess.run([res_desired, res_actual])
+        self.assertAllClose(res_desired_val, res_actual_val, atol=1e-5, rtol=1e-5)
+
 
 if __name__ == "__main__":
   tf.test.main()
