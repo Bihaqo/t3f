1b8faac5101d5cfe54598506eb989d04899c0017
Alexander Novikov <novikov@bayesgroup.ru>
2017-12-12 19:49:47 +0300

    Make max_tt_ranks robust to overflows

--- t3f/utils.py
+++ t3f/utils.py
@@ -39,6 +39,23 @@ def replace_tf_svd_with_np_svd():
   tf.svd = my_svd
 
 
+def robust_cumprod(arr):
+  """Cumulative product with large values replaced by the MAX_DTYPE.
+  
+  robust_cumprod([10] * 100) = [10, 100, 1000, ..., MAX_INT, ..., MAX_INT] 
+  """
+
+  res = np.ones(arr.size, dtype=arr.dtype)
+  change_large_to = np.iinfo(arr.dtype).max
+  res[0] = arr[0]
+  for i in range(1, arr.size):
+    next_value = np.array(res[i - 1]) * np.array(arr[i])
+    if next_value / np.array(arr[i]) != np.array(res[i - 1]):
+      next_value = change_large_to
+    res[i] = next_value
+  return res
+
+
 def max_tt_ranks(raw_shape):
   """Maximal TT-ranks for a TT-object of given shape.
   
@@ -52,11 +69,12 @@ def max_tt_ranks(raw_shape):
   Returns:
     tt_ranks: an integer vector, maximal tt-rank for each dimension
   """
+  raw_shape = np.array(raw_shape).astype(np.int64)
   d = raw_shape.size
-  tt_ranks = np.zeros(d + 1, dtype='int32')
+  tt_ranks = np.zeros(d + 1, dtype='int64')
   tt_ranks[0] = 1
   tt_ranks[d] = 1
-  left_to_right = np.cumprod(raw_shape)
-  right_to_left = np.cumprod(raw_shape[::-1])[::-1]
+  left_to_right = robust_cumprod(raw_shape)
+  right_to_left = robust_cumprod(raw_shape[::-1])[::-1]
   tt_ranks[1:-1] = np.minimum(left_to_right[:-1], right_to_left[1:])
   return tt_ranks
