42aceb41785e223507d5bcb234438f31788f00c0
tt-embedding <email@example.com>
2019-01-28 22:54:36 +0300

    added auto shapes

--- t3f/nn.py
+++ t3f/nn.py
@@ -6,17 +6,125 @@ from keras.engine.topology import Layer
 from keras.layers import Activation
 import t3f
 import tensorflow as tf
+from sympy.utilities.iterables import multiset_partitions
+from sympy.ntheory import factorint
+from itertools import cycle, islice
+from scipy.stats import entropy
+
+
+MODES = ['ascending', 'descending', 'mixed']
+CRITERIONS = ['entropy', 'var']
+
+
+def _to_list(p):
+  res = []
+  for k, v in p.items():
+      res += [k, ] * v
+  return res
+
+
+def _roundup(n, k):
+  return int(np.ceil(n / 10**k)) * 10**k
+
+
+def _roundrobin(*iterables):
+  "roundrobin('ABC', 'D', 'EF') --> A D E B F C"
+  # Recipe credited to George Sakkis
+  pending = len(iterables)
+  nexts = cycle(iter(it).__next__ for it in iterables)
+  while pending:
+      try:
+          for next in nexts:
+              yield next()
+      except StopIteration:
+          pending -= 1
+          nexts = cycle(islice(nexts, pending))
+
+
+def _get_all_factors(n, d=3, mode='ascending'):
+  p = _factorint2(n)
+  if len(p) < d:
+      p = p + [1, ] * (d - len(p))
+
+  if mode == 'ascending':
+      def prepr(x):
+          return tuple(sorted([np.prod(_) for _ in x]))
+  elif mode == 'descending':
+      def prepr(x):
+          return tuple(sorted([np.prod(_) for _ in x], reverse=True))
+
+  elif mode == 'mixed':
+      def prepr(x):
+          x = sorted(np.prod(_) for _ in x)
+          N = len(x)
+          xf, xl = x[:N//2], x[N//2:]
+          return tuple(_roundrobin(xf, xl))
+
+  else:
+      raise ValueError('Wrong mode specified, only {} are available'.format(MODES))
+
+  raw_factors = multiset_partitions(p, d)
+  clean_factors = [prepr(f) for f in raw_factors]
+  clean_factors = list(set(clean_factors))
+  return clean_factors
+
+
+def _factorint2(p):
+  return _to_list(factorint(p))
+
+
+def auto_shape(n, d=3, criterion='entropy', mode='ascending'):
+  factors = _get_all_factors(n, d=d, mode=mode)
+  if criterion == 'entropy':
+      weights = [entropy(f) for f in factors]
+  elif criterion == 'var':
+      weights = [-np.var(f) for f in factors]
+  else:
+      raise ValueError('Wrong criterion specified, only {} are available'.format(CRITERIONS))
+
+  i = np.argmax(weights)
+  return list(factors[i])
+
+
+def suggest_shape(n, d=3, criterion='entropy', mode='ascending'):
+  weights = []
+  for i in range(len(str(n))):
+
+      n_i = _roundup(n, i)
+      if criterion == 'entropy':
+          weights.append(entropy(auto_shape(n_i, d=d, mode=mode, criterion=criterion)))
+      elif criterion == 'var':
+          weights.append(-np.var(auto_shape(n_i, d=d, mode=mode, criterion=criterion)))
+      else:
+          raise ValueError('Wrong criterion specified, only {} are available'.format(CRITERIONS))
+
+  i = np.argmax(weights)
+  factors = auto_shape(int(_roundup(n, i)), d=d, mode=mode, criterion=criterion)
+  return factors
 
 
 class KerasDense(Layer):
   _counter = count(0)
 
-  def __init__(self, input_dims, output_dims, tt_rank=2,
+  def __init__(self, in_dim=None, out_dim=None, d=None, mode='mixed',
+               criterion='entropy',input_dims=None, output_dims=None, tt_rank=8,
                activation=None, use_bias=True, kernel_initializer='glorot',
                bias_initializer=0.1, **kwargs):
     """Creates a TT-Matrix based Dense Keras layer.
+    If in_dim, out_dim and d are provided, will determine the (quasi)optimal
+    factorizations automatically using 'mode' factorization style, and 'criterion'
+    for optimality. Default settings are recommended.
+    Otherwise, the desired factorizations has to be specified as input_dims
+    and output_dims lists.
 
     Args:
+      in_dim: an int, number of input neurons
+      out_dim: an int, number of output neurons
+      d: number of factors in shape factorizations
+      mode: string, specifies the way of factorizing in_dim and out_dim.
+          Possible values are 'ascending', 'descending', 'mixed'.
+      criterion: string, specifies the shape optimality criterion.
+          Possible values are 'entropy', 'var'.
       input_dims: an array, tensor shape of the matrix row index
       ouput_dims: an array, tensor shape of the matrix column index
       tt_rank: a number or an array, desired tt-rank of the TT-Matrix
@@ -36,6 +144,10 @@ class KerasDense(Layer):
         unknown.
     """
     self.counter = next(self._counter)
+    if in_dim and out_dim and d:
+      input_dims = auto_shape(in_dim, d=d, mode=mode, criterion=criterion)
+      output_dims = auto_shape(out_dim, d=d, mode=mode, criterion=criterion)
+
     self.tt_shape = [input_dims, output_dims]
     self.output_dim = np.prod(output_dims)
     self.tt_rank = tt_rank
