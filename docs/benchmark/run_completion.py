6260050b2e6654c12d30c6589724fda14efcd5aa
Alexander Novikov <anovikov@anovikov-macbookpro4.roam.corp.google.com>
2019-06-11 12:56:02 +0100

    small fixes: do not repeat work, identation fix, print current settings

--- docs/benchmark/run_completion.py
+++ docs/benchmark/run_completion.py
@@ -4,4 +4,4 @@ import subprocess
 for n in [20, 100, 500]:
   for d in [10, 20, 30]:
     for r in [5, 10, 20]:
-		print(subprocess.check_output(['python3', 'completion.py', '--n=%d' % n, '--d=%d' % d, '--r=%d' % r]))
+      print(subprocess.check_output(['python3', 'completion.py', '--n=%d' % n, '--d=%d' % d, '--r=%d' % r]))
