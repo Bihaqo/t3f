556d2debfe91b0597a4b603f5ca3a468814f3bd3
Alexander Novikov <sasha.v.novikov@gmail.com>
2019-10-23 14:13:52 +0100

    better hyperparams

--- docs/benchmark/run_all_autodiff.py
+++ docs/benchmark/run_all_autodiff.py
@@ -27,12 +27,12 @@ def run_single(case, n, d, r, R=None):
     pass
 
 
-for n in [20, 100, 500]:
+for n in [5, 10, 20]:
   for d in [10, 20, 40]:
-    for r in [5, 10, 20]:
+    for r in [5, 10, 20, 40]:
       run_single('completion', n, d, r)
       run_single('ExpMachines', n, d, r)
-      for R in [5, 10, 20]:
+      for R in [5, 10, 20, 40]:
         run_single('xAx', n, d, r, R)
         run_single('xABx', n, d, r, R)
         run_single('RayleighQuotient', n, d, r, R)
