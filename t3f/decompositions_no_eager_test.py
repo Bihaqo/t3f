5c2ce99827282c585b97e392cc5dcf251d753893
Alexander Novikov <sasha.v.novikov@gmail.com>
2020-02-23 17:10:50 +0000

    Do not test converting a placeholder with unknown shape into TT, does not worth it

--- t3f/decompositions_no_eager_test.py
+++ t3f/decompositions_no_eager_test.py
@@ -11,23 +11,6 @@ from t3f import decompositions
 class _DecompositionsTest():
 
   def testTTTensor(self):
-    shape = (2, 1, 4, 3)
-    np.random.seed(1)
-    with tf.Session() as sess:
-      tens = np.random.rand(*shape).astype(self.dtype.as_numpy_dtype)
-      tf_tens = tf.constant(tens)
-      tt_tens = decompositions.to_tt_tensor(tf_tens, max_tt_rank=3)
-      static_tt_ranks = tt_tens.get_tt_ranks().as_list()
-
-      # Try to decompose the same tensor with unknown shape.
-      tf_tens_pl = tf.placeholder(self.dtype, (None, None, 4, None))
-      tt_tens = decompositions.to_tt_tensor(tf_tens_pl, max_tt_rank=3)
-      tt_val = ops.full(tt_tens).eval({tf_tens_pl: tens})
-      self.assertAllClose(tens, tt_val)
-      dynamic_tt_ranks = shapes.tt_ranks(tt_tens).eval({tf_tens_pl: tens})
-      self.assertAllEqual(dynamic_tt_ranks, static_tt_ranks)
-
-  def testTTTensorSimple(self):
     # Test that a tensor of ones and of zeros can be converted into TT with
     # TT-rank 1.
     shape = (2, 1, 4, 3)
