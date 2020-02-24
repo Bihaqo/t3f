450ecfc47053a0cabc411b292f3cae7fc16fc724
Alexander Novikov <sasha.v.novikov@gmail.com>
2020-02-24 16:31:27 +0000

    Make replace_tf_svd_with_np_svd work in eager mode

--- t3f/utils.py
+++ t3f/utils.py
@@ -8,7 +8,7 @@ def unravel_index(indices, shape):
   with tf.name_scope('unravel_index'):
     indices = tf.expand_dims(indices, 0)
     shape = tf.expand_dims(shape, 1)
-    strides_shifted = tf.cumprod(shape, exclusive=True, reverse=True)
+    strides_shifted = tf.math.cumprod(shape, exclusive=True, reverse=True)
     res = (indices // strides_shifted) % shape
     return tf.transpose(res, (1, 0))
 
@@ -20,25 +20,17 @@ def replace_tf_svd_with_np_svd():
   if hasattr(tf, 'original_svd'):
     # This function has been already called and tf.svd is already replaced.
     return
-  tf.original_svd = tf.svd
+  tf.original_svd = tf.linalg.svd
 
   def my_svd(tensor, full_matrices=False, compute_uv=True):
     dtype = tensor.dtype
-    u, s, v = tf.py_func(np.linalg.svd, [tensor, full_matrices, compute_uv],
-                         [dtype, dtype, dtype])
-    s_, u_, v_ = tf.original_svd(tensor, full_matrices, compute_uv)
-    s = tf.reshape(s, s_.get_shape())
-    u = tf.reshape(u, u_.get_shape())
-    v_shape = v_.get_shape().as_list()
-    v_shape[-2], v_shape[-1] = v_shape[-1], v_shape[-2]
-    v = tf.reshape(v, v_shape)
+    u, s, v = np.linalg.svd(tensor, full_matrices, compute_uv)
     # Converting numpy order of v dims to TF order.
     order = list(range(tensor.get_shape().ndims))
     order[-2], order[-1] = order[-1], order[-2]
     v = tf.transpose(v, order)
-    return s, u, v
+    return tf.constant(s), tf.constant(u), v
 
-  tf.svd = my_svd
   tf.linalg.svd = my_svd
 
 
